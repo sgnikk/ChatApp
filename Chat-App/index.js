91d901ea9eb96ff740266f98c7c7657e59c0f782
sgnikk <dominicsaga02@gmail.com>
2025-05-03 18:38:45 +0800

    added a style for create account form and login form

--- Chat-App/index.js
+++ Chat-App/index.js
@@ -1,3 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+const firebaseConfig = {
+  apiKey: "AIzaSyAKBWqwNj7IZedngwAmXo7pyYJDayHBlHw",
+  authDomain: "chat-app-33908.firebaseapp.com",
+  databaseURL: "https://chat-app-33908-default-rtdb.firebaseio.com",
+  projectId: "chat-app-33908",
+  storageBucket: "chat-app-33908.appspot.com",
+  messagingSenderId: "1023503771082",
+  appId: "1:1023503771082:web:723b90204b9c24d4021b33"
+};
+
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 const messagesRef = db.ref("messages");
@@ -50,3 +61,4 @@ messagesRef.on("child_added", snapshot => {
     messagesEl.appendChild(container);
     messagesEl.scrollTop = messagesEl.scrollHeight;
   });
+})
